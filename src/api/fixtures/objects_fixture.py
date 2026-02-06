afb2ea78889e0a07b48f3b144e305644827b7899
Evgeny Danilovich <evgeny.danilovich@siliconmint.com>
2026-02-06 23:48:27 +0300

    remove unused code

--- src/api/fixtures/objects_fixture.py
+++ src/api/fixtures/objects_fixture.py
@@ -12,10 +12,14 @@ from src.api.models.responses.create_user_response import CreateUserResponse
 def cleanup_object(objects: list):
     api_manager = ApiManager(objects)
 
-    delete_objects_by_uuid(api_manager, objects, PatientCreateResponse, 'delete_patient')
-    delete_objects_by_uuid(api_manager, objects, CreateProviderResponse, 'delete_provider')
-    delete_objects_by_uuid(api_manager, objects, CreateUserResponse, 'delete_user')
-    delete_objects_by_uuid(api_manager, objects, CreatePersonResponse, 'delete_person')
+    cleanup_tasks = [
+        (PatientCreateResponse, 'delete_patient'),
+        (CreateProviderResponse, 'delete_provider'),
+        (CreateUserResponse, 'delete_user'),
+        (CreatePersonResponse, 'delete_person')
+    ]
+    for resp_class, method_name in cleanup_tasks:
+        delete_objects_by_uuid(api_manager, objects, resp_class, method_name)
     # for obj in objects:
     #     if isinstance(obj, CreatePersonResponse):
     #         api_manager.admin_steps.delete_person(obj.uuid)
@@ -31,32 +35,9 @@ def created_objects():
     yield objects
     cleanup_object(objects)
 
-def delete_users(api_manager: ApiManager, objects: list):
-    users = [obj for obj in objects if isinstance(obj, CreateUserResponse)]
-    for user in users:
-        api_manager.user_steps.delete_user(user.uuid)
-
-def delete_providers(api_manager: ApiManager, objects: list):
-    providers = [obj for obj in objects if isinstance(obj, CreateProviderResponse)]
-    for provider in providers:
-        api_manager.user_steps.delete_provider(provider.uuid)
-
-def delete_patients(api_manager: ApiManager, objects: list):
-    patients = [obj for obj in objects if isinstance(obj, PatientCreateResponse)]
-    for patient in patients:
-        api_manager.user_steps.delete_patient(patient.uuid)
-
-def delete_persons(api_manager: ApiManager, objects: list):
-    persons = [obj for obj in objects if isinstance(obj, CreatePersonResponse)]
-    for person in persons:
-        api_manager.user_steps.delete_person(person.uuid)
-
-
 def delete_objects_by_uuid(api_manager: ApiManager, objects: list, response_type: type, delete_method_name: str):
     """
-    Универсальный метод удаления.
-    :param response_type: Класс ответа (например, PatientCreateResponse)
-    :param delete_method_name: Имя метода в user_steps (например, 'delete_patient')
+        Универсальный метод удаления cущностей
     """
     target_objects = [obj for obj in objects if isinstance(obj, response_type)]
     delete_func = getattr(api_manager.user_steps, delete_method_name)
