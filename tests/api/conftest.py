a5e79bfbc7b7e5d6575e8ff2920f13ed511cfc98
karmise <yucant3@gmail.com>
2026-02-13 19:47:19 +0600

    Import fixtures

---
+++ tests/api/conftest.py
@@ -0,0 +1,4 @@
+from src.api.fixtures.user_fixtures import *
+from src.api.fixtures.api_fixtures import *
+from src.api.fixtures.objects_fixture import *
+from src.api.fixtures.visit_fixtures import *
