3c17d1345aade80dcbe503e72896c41156fb64e3
Evgeny Danilovich <evgeny.danilovich@siliconmint.com>
2026-02-14 10:48:41 +0300

    move patient assertion fixtures

--- conftest.py
+++ conftest.py
@@ -1,8 +1,9 @@
 from src.api.fixtures.user_fixtures import *
 from src.api.fixtures.api_fixtures import *
 from src.api.fixtures.objects_fixture import *
-from src.api.fixtures.assertion_fixtures import *
 from src.api.fixtures.visit_fixtures import *
+from src.api.fixtures.assertion_fixtures.patient_assertion_fixtures import *
+from src.api.fixtures.assertion_fixtures.visit_assertion_fixtures import *
 
 
 import time
