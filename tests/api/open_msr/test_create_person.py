ad855c61046208edcaa1c01990ac889f98d29641
ekonchina <ekaterina-konchina@yandex.ru>
2026-02-14 06:53:52 +0300

    fix

--- tests/api/open_msr/test_create_person.py
+++ tests/api/open_msr/test_create_person.py
@@ -12,36 +12,24 @@ def test_create_person(api_manager):
     PersonCreationVerifier(api_manager).verify_person_created(person.uuid, expected_request=create_person_request)
 
 
-@pytest.mark.xfail #TODO: how fail by parametrized test
+#@pytest.mark.xfail #TODO: how fail by parametrized test
 @pytest.mark.api
 @pytest.mark.parametrize(
-    "payload, error_key, error_value",
+    "field, value, error_key, error_value",
     [
         (
-            CreatePersonInvalidRequest(
-                names=[{"givenName": "John", "familyName": "Doe"}],
-                gender="X",  # invalid (дока: M/F/U)
-                birthdate="1997-09-02",
-                addresses=[{"address1": "Street 1", "cityVillage": "City", "country": "CH", "postalCode": "8000"}],
-            ),
+            "gender","X",  # invalid (дока: M/F/U)
             "error",
             "gender",
         ),
-        (
-            CreatePersonInvalidRequest(
-                names=[{"givenName": "John", "familyName": "Doe"}],
-                gender="M",
-                birthdate="1997-99-99",  # invalid date
-                addresses=[{"address1": "Street 1", "cityVillage": "City", "country": "CH", "postalCode": "8000"}],
-            ),
-            "error",
-            "birth",
-        ),
     ],
 )
-def test_create_person_invalid(api_manager, payload, error_key, error_value):
+def test_create_person_invalid(api_manager, field, value, error_key, error_value):
+    create_person_request = RandomModelGenerator.generate(CreatePersonRequest)
+    setattr(create_person_request, field, value)
+
     api_manager.user_steps.create_invalid_person(
-        create_person_request=payload,
+        create_person_request=create_person_request,
         error_key=error_key,
         error_value=error_value,
     )
