211539b8d09264f1c20551918a4d911909ce6376
dileen-mollis <mullakhanovad@gmail.com>
2026-03-02 04:25:15 +0500

    fix: add RandomModelGenerator, delete comments, chain

--- tests/ui/test_search_patient_by_admin.py
+++ tests/ui/test_search_patient_by_admin.py
@@ -2,12 +2,8 @@ import re
 
 import pytest
 from playwright.sync_api import expect, Page
-
 from src.api.classes.api_manager import ApiManager
 from src.api.generators.random_model_generator import RandomModelGenerator
-from src.api.models.requests.create_patient_request import CreatePatientRequest
-from src.api.models.responses.create_patient_response import PatientCreateResponse
-
 from src.ui.open_msr_home_page import OpenMsrHomePage
 from src.ui.patient_pages.patient_summery_page import PatientSummaryPage
 
@@ -17,7 +13,6 @@ from src.ui.patient_pages.patient_summery_page import PatientSummaryPage
 @pytest.mark.admin_session
 class TestSearchPatientByAdmin:
 
-    #  Search by Fullname
     def test_search_patient_by_fullname_by_admin(
         self,
         page: Page,
@@ -48,27 +43,22 @@ class TestSearchPatientByAdmin:
             .should_have_patient(given, family)
 
 
-
-    #  Search by Identifier
     def test_search_patient_by_id_by_admin(
         self,
         page: Page,
         api_manager
     ):
-        # --- Create patient ---
         request = api_manager.user_steps.build_create_patient_request()
         response = api_manager.user_steps.create_patient(request)
 
         identifier = request.identifiers[0].identifier
 
-        # --- Search by ID ---
         OpenMsrHomePage(page).open().wait_until_loaded() \
             .header_search \
             .open_search() \
             .search(identifier) \
             .select_first_result()
 
-        # --- Verify redirect ---
         expect(page).to_have_url(
             re.compile(rf"/patient/{response.uuid}/chart")
         )
@@ -76,17 +66,16 @@ class TestSearchPatientByAdmin:
         PatientSummaryPage(page, patient_uuid=response.uuid) \
             .should_be_opened()
 
-    #  Search non-existent patient
     def test_search_patient_by_nonexistent_fullname_by_admin(
-        self,
-        page: Page,
+            self,
+            page: Page,
     ):
-        fake_name = "ZZZ Nonexistent Patient"
+        fake_name = RandomModelGenerator.generate(str)
 
-        home = OpenMsrHomePage(page).open().wait_until_loaded()
-
-        search_component = home.header_search \
+        OpenMsrHomePage(page) \
+            .open() \
+            .wait_until_loaded() \
+            .header_search \
             .open_search() \
-            .search(fake_name)
-
-        search_component.should_have_no_results()
+            .search(fake_name) \
+            .should_have_no_results()
