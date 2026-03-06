da1327eed8c8f0024bf158cd435036d5899dd681
Evgeny Danilovich <evgeny.danilovich@siliconmint.com>
2026-03-06 17:27:33 +0300

    remove no used code

--- src/ui/initial_setup_page.py
+++ src/ui/initial_setup_page.py
@@ -14,19 +14,6 @@ class InitialSetupPage(BasePage):
     def url(self) -> str:
         return "/initialsetup#"
 
-    def get_row_with_text(self, text: str):
-        return self.page.locator('.task-item').filter(has_text=text)
-
-    def wait_for_progress(self, task_name: str, progress: int):
-        if not (1 <= progress <= 100):
-            raise AssertionError('progress should be between 1 and 100')
-
-        row_locator = self.get_row_with_text(task_name)
-        progress_bar = row_locator.locator(".progress-bar[role='progressbar']")
-        expect(progress_bar).to_have_attribute("aria-valuenow", f"{progress}", timeout=120_000)
-
-        return self
-
     def wait_for_setup_to_be_finished(self):
         def is_finished(response):
             if "openmrs/initialsetup" in response.url and response.status == 200:
